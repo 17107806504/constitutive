5f19cf2c9ca9c7c9b108c9ac41b812a8e545cbae
17107806504 <55691199+17107806504@users.noreply.github.com>
2022-06-14 16:17:42 +0800

    Add files via upload

---
+++ 0.001-400.py
@@ -0,0 +1,23 @@
+import numpy as np #导入numpy工具包
+import xlrd
+import date_function as df
+import graphplot as plts2
+
+
+
+
+path=r'E:\date\科研\数据收集\5052\5052等温拉伸-准静态应变率0.001\5052等温拉伸-准静态应变率0.001\5052-400-1.xls'
+trueStrain1,trueStress1=df.calTrueValue(path,1,22,12000)
+path=r'E:\date\科研\数据收集\5052\5052等温拉伸-准静态应变率0.001\5052等温拉伸-准静态应变率0.001\5052-400-2.xls'
+trueStrain2,trueStress2=df.calTrueValue(path,1,22,12000)
+
+plts2.autoPlot_test([trueStrain1,trueStrain2],[trueStress1,trueStress2],['1','2'],title='400度 0.001 1/s')
+x=[]
+
+for i in range(550):
+    x.append(i*0.001)
+y1=np.interp(x, trueStrain1, trueStress1)
+y2=np.interp(x, trueStrain2, trueStress2)
+y=(np.array(y1)+np.array(y2))/2
+
+plts2.autoPlot_test([x],[y],['mean'],title='400度 0.001 1/s')
